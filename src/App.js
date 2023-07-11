d62575ee152a37957a6da3abab86fdef71db3920
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-07-11 11:58:24 +0300

    Add elements to the route.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import Booklist from './components/Booklist';
+import Bookstate from './components/Bookstate';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" />
-      <Route path="/catagories" />
+      <Route path="/" element={<Booklist />} />
+      <Route path="/catagories" element={<Bookstate />} />
     </Routes>
   );
 }
